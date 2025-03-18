da31336ae239fa2a65b90f352028b7baed7811ae
KozTGit <greenwaygloballt@gmail.com>
2025-03-18 16:59:51 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`El amigo secreto es:`);
+        //document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        //El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','Maria');
+        } else {
+            asignarTextoElemento('p','El nombre no puede estar vacio');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarTexto() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p','El amigo secreto es:');
+    } else {
+        //Si el numero generado está incluido en la lista 
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function datosIniciales() {
+    asignarTextoElemento('h1','Juego Amigo secreto!');
+    asignarTextoElemento('p',`Escribe los nombres de tus amigos, maximo ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
+}
+
+function reiniciarJuego() {
+    
+    limpiarTexto();
+   
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    //document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
+}
+
+datosIniciales();
